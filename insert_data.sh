7bb24869734d8a33341c8901e865044c9d7e463d
Uday Sai <udaysai629@gmail.com>
2024-03-14 08:29:05 +0000

    Save progress: 564.1

--- insert_data.sh
+++ insert_data.sh
@@ -5,7 +5,8 @@ PSQL="psql -X --username=freecodecamp --dbname=students --no-align --tuples-only
 cat courses.csv | while IFS="," read MAJOR COURSE
 do 
   # get major_id
-  MAJOR_ID=$($PSQL "SELECT major_id FROM MAJORS WHERE major='$MAJOR'")
+  MAJOR_ID=$($PSQL "SELECT major_id FROM majors WHERE major='$MAJOR'")
+  echo $MAJOR_ID
   # if not found
 
   # insert major
