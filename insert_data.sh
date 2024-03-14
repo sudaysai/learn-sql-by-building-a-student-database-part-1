38fa80413e9a2ad125ea5c47dd78165370de919b
Uday Sai <udaysai629@gmail.com>
2024-03-14 08:24:12 +0000

    Save progress: 550.1

--- insert_data.sh
+++ insert_data.sh
@@ -3,5 +3,21 @@
 # Script to insert data from courses.csv and students.csv into students database
 cat courses.csv | while IFS="," read MAJOR COURSE
 do 
-  
+  # get major_id
+
+  # if not found
+
+  # insert major
+
+  # get new major_id
+
+  # get course_id
+
+  # if not found
+
+  # insert course
+
+  # get new course_id
+
+  # insert into majors_courses
 done
